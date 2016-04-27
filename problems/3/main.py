615933f02b7d6add36ee9bb4a6684f4bbd018fa3
Dylan Allbee <dylan@allbee.org>
2016-04-27 09:28:19 -0700

    Refactored with an almost trivial recursive

--- problems/3/main.py
+++ problems/3/main.py
@@ -4,22 +4,14 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 """
 
-import math
-
-
-def is_prime(n):
-    for x in range(2, n):
-        if n % x == 0:
-            return False
-    return True
+from math import sqrt
 
 
 def factorize(n):
-    primes = []
-    for x in range(2, int(math.sqrt(n)), 2):
-        if n % x == 0 and is_prime(x):
-            primes.append(x)
-    return primes
+    for x in range(2, int(sqrt(n)) + 1):
+        if n % x == 0:
+            return {x} | factorize(n // x)
+    return {n}
 
 
 print(max(factorize(600851475143)))
